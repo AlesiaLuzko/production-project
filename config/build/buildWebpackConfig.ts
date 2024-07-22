4e4d013d9ef46649f2272b8a309a456174c97c77
Alesia Luzko <lesya.grishko@gmail.com>
2024-07-22 18:55:48 +0200

    webpack-dev-server configuration

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,14 +1,14 @@
-import { BuildOptions } from './types/config'
 import webpack from 'webpack'
-import path from 'path'
-import { buildPlaging } from './buildPlugins'
+import { buildDevServer } from './buildDevServer'
 import { buildLoaders } from './buildLoaders'
+import { buildPlaging } from './buildPlugins'
 import { buildResolvers } from './buildResolvers'
+import { BuildOptions } from './types/config'
 
 export function buildWebpackConfig(
 	options: BuildOptions
 ): webpack.Configuration {
-	const { paths, mode } = options
+	const { paths, mode, isDev } = options
 
 	return {
 		mode,
@@ -23,5 +23,7 @@ export function buildWebpackConfig(
 			rules: buildLoaders(),
 		},
 		resolve: buildResolvers(),
+		devtool: isDev ? 'inline-source-map' : undefined,
+		devServer: isDev ? buildDevServer(options) : undefined,
 	}
 }
