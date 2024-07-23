d607aec66afc03d5338fffb2bedd992bc8acd232
Alesia Luzko <lesya.grishko@gmail.com>
2024-07-23 20:13:37 +0200

    configuration css modules

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Counter } from './components/Counter'
+import './index.scss'
+
+const App = () => {
+	return (
+		<div className='app'>
+			Welcome to HELL
+			<Counter />
+		</div>
+	)
+}
+
+export default App
