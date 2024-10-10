8a40bba39dd2142f314651cb020d381b9e2e55a3
Alesia Luzko <lesya.grishko@gmail.com>
2024-10-10 19:16:17 +0200

    add navbar.stories

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,6 +1,5 @@
 import { Story } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
-import 'app/styles/index.scss';
 
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => {
 	return (
