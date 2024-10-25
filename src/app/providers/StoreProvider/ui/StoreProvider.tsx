2ffe8ec80990cf8ff1e5b64de7910839a2aa039b
Alesia Luzko <lesya.grishko@gmail.com>
2024-10-25 16:57:32 +0200

    add tests for counter, add translations

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -2,17 +2,18 @@ import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { createReduxStore } from '../config/store';
 import { StateSchema } from '../config/StateSchema';
+import { DeepPartial } from '@reduxjs/toolkit';
 
 interface StoreProviderProps {
 	children?: ReactNode;
-	initialState?: StateSchema;
+	initialState?: DeepPartial<StateSchema>;
 }
 
 export const StoreProvider = ({
 	children,
 	initialState,
 }: StoreProviderProps) => {
-	const store = createReduxStore(initialState);
+	const store = createReduxStore(initialState as StateSchema);
 
 	return <Provider store={store}>{children}</Provider>;
 };
