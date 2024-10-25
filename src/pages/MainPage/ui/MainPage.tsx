bc2b54231e11fc8490e9fce0a9682151b261d201
Alesia Luzko <lesya.grishko@gmail.com>
2024-10-25 16:08:47 +0200

    add counter, redux-toolkit

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,15 @@
+import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
 	const { t } = useTranslation('main');
 
-	return <div>{t('Main page')}</div>;
+	return (
+		<div>
+			{t('Main page')}
+			<Counter />
+		</div>
+	);
 };
 
 export default MainPage;
